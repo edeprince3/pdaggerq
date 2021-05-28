9b816de6483c09daebe7ddc9b98d0cd27ce2ac58
Eugene DePrince <deprince@chem.fsu.edu>
2021-05-28 09:39:56 -0400

    updates ccsd_energy.py and ccsd_singles.py to use new add_st_operator function

--- samples/ccsd_singles.py
+++ samples/ccsd_singles.py
@@ -13,66 +13,59 @@ print('')
 print('    < 0 | m* e e(-T) H e(T) | 0> :')
 print('')
 
-# one-electron part: need only up to double commutators
-# h
-ahat.add_operator_product(1.0,['h(p,q)'])
+## one-electron part: need only up to double commutators
+## h
+#ahat.add_operator_product(1.0,['h(p,q)'])
+#
+## [h, T1]
+#ahat.add_commutator(1.0,['h(p,q)','t1(a,i)'])
+#
+## [h, T2]
+#ahat.add_commutator(1.0,['h(p,q)','t2(a,b,i,j)'])
+#
+## [[h, T1], T1]]
+#ahat.add_double_commutator(0.5, ['h(p,q)','t1(a,i)','t1(b,j)'])
+#
+## [[h, T1], T2]] + [[h, T2], T1]]
+#ahat.add_double_commutator( 1.0, ['h(p,q)','t1(a,i)','t2(c,d,k,l)'])
+#
+## [[h, T2, T2]]
+#ahat.add_double_commutator( 0.5, ['h(p,q)','t2(a,b,i,j)','t2(c,d,k,l)'])
+#
+## two-electron part: need up to quadruple commutators
+#
+## g
+#ahat.add_operator_product(1.0,['g(p,r,q,s)'])
+#
+## [g, T1]
+#ahat.add_commutator(1.0,['g(p,r,q,s)','t1(a,i)'])
+#
+## [g, T2]
+#ahat.add_commutator(1.0,['g(p,r,q,s)','t2(a,b,i,j)'])
+#
+## [[g, T1], T1]]
+#ahat.add_double_commutator(0.5, ['g(p,r,q,s)','t1(a,i)','t1(b,j)'])
+#
+## [[g, T1], T2]] + [[g, T2], T1]]
+#ahat.add_double_commutator( 1.0, ['g(p,r,q,s)','t1(a,i)','t2(c,d,k,l)'])
+#
+## [[g, T2, T2]]
+#ahat.add_double_commutator( 0.5, ['g(p,r,q,s)','t2(a,b,i,j)','t2(c,d,k,l)'])
+#
+## triple commutators
+#
+## [[[g, T1, T1], T1]
+#ahat.add_triple_commutator( 1.0/6.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t1(c,k)'])
+#
+## [[[g, T1, T1], T2] + [[[g, T1, T2], T1] + [[[g, T2, T1], T1]
+#ahat.add_triple_commutator( 1.0/2.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t2(c,d,k,l)'])
+#
+## [[[[g, T1], T1], T1], T1]
+#ahat.add_quadruple_commutator( 1.0/24.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t1(c,k)','t1(d,l)'])
 
-# [h, T1]
-ahat.add_commutator(1.0,['h(p,q)','t1(a,i)'])
 
-# [h, T2]
-ahat.add_commutator(1.0,['h(p,q)','t2(a,b,i,j)'])
-
-# [[h, T1], T1]]
-ahat.add_double_commutator(0.5, ['h(p,q)','t1(a,i)','t1(b,j)'])
-
-# [[h, T1], T2]] + [[h, T2], T1]]
-ahat.add_double_commutator( 1.0, ['h(p,q)','t1(a,i)','t2(c,d,k,l)'])
-
-# [[h, T2, T2]]
-ahat.add_double_commutator( 0.5, ['h(p,q)','t2(a,b,i,j)','t2(c,d,k,l)'])
-
-# two-electron part: need up to quadruple commutators
-
-# g
-ahat.add_operator_product(1.0,['g(p,r,q,s)'])
-
-# [g, T1]
-ahat.add_commutator(1.0,['g(p,r,q,s)','t1(a,i)'])
-
-# [g, T2]
-ahat.add_commutator(1.0,['g(p,r,q,s)','t2(a,b,i,j)'])
-
-# [[g, T1], T1]]
-ahat.add_double_commutator(0.5, ['g(p,r,q,s)','t1(a,i)','t1(b,j)'])
-
-# [[g, T1], T2]] + [[g, T2], T1]]
-ahat.add_double_commutator( 1.0, ['g(p,r,q,s)','t1(a,i)','t2(c,d,k,l)'])
-
-# [[g, T2, T2]]
-ahat.add_double_commutator( 0.5, ['g(p,r,q,s)','t2(a,b,i,j)','t2(c,d,k,l)'])
-
-# triple commutators
-
-# [[[g, T1, T1], T1]
-ahat.add_triple_commutator( 1.0/6.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t1(c,k)'])
-
-# [[[g, T1, T1], T2] + [[[g, T1, T2], T1] + [[[g, T2, T1], T1]
-ahat.add_triple_commutator( 1.0/2.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t2(c,d,k,l)'])
-
-# [[[g, T1, T2], T2] + [[[g, T2, T1], T2] + [[[g, T2, T2], T1]
-#ahat.add_triple_commutator( 1.0/2.0, ['g(p,r,q,s)','t1(a,i)','t2(c,d,k,l)','t2(e,f,m,n)'])
-
-# [[[g, T2, T2], T2]
-#ahat.add_triple_commutator( 1.0/6.0, ['g(p,r,q,s)','t2(a,b,i,j)','t2(c,d,k,l)','t2(e,f,m,n)'])
-
-# quadruple commutators: the only one that yields non-zero contributions to the doubles equations will be
-
-# [[[[g, T1], T1], T1], T1]
-ahat.add_quadruple_commutator( 1.0/24.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t1(c,k)','t1(d,l)'])
-
-# [[[[g, T1], T1], T1], T2]
-#ahat.add_quadruple_commutator( 1.0/24.0, ['g(p,r,q,s)','t1(a,i)','t1(b,j)','t1(c,k)','t2(e,f,m,n)'])
+ahat.add_st_operator(1.0,['h','t1','t2'])
+ahat.add_st_operator(1.0,['g','t1','t2'])
 
 ahat.simplify()
 
