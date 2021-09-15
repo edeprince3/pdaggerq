3a825db7b044211076e6d01efed48aab7c228749
deprince <eugene.deprince@gmail.com>
2021-09-15 16:27:07 -0400

    toy code to diagonalize full eom-ccsd hamiltonian

--- examples/eom_ccsd_hamiltonian.py
+++ examples/eom_ccsd_hamiltonian.py
@@ -1,65 +1,216 @@
 
 import pdaggerq
 
-pq = pdaggerq.pq_helper("fermi")
+from pdaggerq.parser import contracted_strings_to_tensor_terms
 
-# set right and left-hand operators
-pq.set_left_operators([['e1(m,e)']])
-pq.set_right_operators([['e1(a,i)']])
+def main():
+    pq = pdaggerq.pq_helper("fermi")
 
-print('')
-print('    H(m,e;i,a) = <0|e1(m,e) e(-T) H e(T) e1(a,i)|0>')
-print('')
+    # set right and left-hand operators
+    pq.set_left_operators([['1']])
+    pq.set_right_operators([['1']])
 
-pq.add_st_operator(1.0,['f'],['t1','t2'])
-pq.add_st_operator(1.0,['v'],['t1','t2'])
+    print('')
+    print('#    H(0;0) = <0| e(-T) H e(T) |0>')
+    print('')
 
-pq.simplify()
-pq.print_fully_contracted()
-pq.clear()
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
 
-# set right and left-hand operators
-pq.set_left_operators([['e1(m,e)']])
-pq.set_right_operators([['e2(a,b,j,i)']])
+    pq.simplify()
 
-print('')
-print('    H(m,e;i,j,a,b) = <0|e1(m,e) e(-T) H e(T) e2(a,b,j,i)|0>')
-print('')
+    H00 = pq.fully_contracted_strings()
+    H00 = contracted_strings_to_tensor_terms(H00)
+    for my_term in H00:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='H00'))
+        print()
 
-pq.add_st_operator(1.0,['f'],['t1','t2'])
-pq.add_st_operator(1.0,['v'],['t1','t2'])
+    pq.clear()
 
-pq.simplify()
-pq.print_fully_contracted()
-pq.clear()
+    # set right and left-hand operators
+    pq.set_left_operators([['e1(m,e)']])
+    pq.set_right_operators([['1']])
 
-# set right and left-hand operators
-pq.set_left_operators([['e2(m,n,f,e)']])
-pq.set_right_operators([['e1(a,i)']])
+    print('')
+    print('#    H(m,e;0) = <0|e1(m,e) e(-T) H e(T) |0>')
+    print('')
 
-print('')
-print('    H(m,n,e,f;i,a) = <0|e2(m,n,f,e) e(-T) H e(T) e1(a,i)|0>')
-print('')
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
 
-pq.add_st_operator(1.0,['f'],['t1','t2'])
-pq.add_st_operator(1.0,['v'],['t1','t2'])
+    pq.simplify()
 
-pq.simplify()
-pq.print_fully_contracted()
-pq.clear()
+    Hs0 = pq.fully_contracted_strings()
+    Hs0 = contracted_strings_to_tensor_terms(Hs0)
+    for my_term in Hs0:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='Hs0',
+                                    output_variables=('e', 'm')))
+        print()
 
-# set right and left-hand operators
-pq.set_left_operators([['e2(m,n,f,e)']])
-pq.set_right_operators([['e2(a,b,j,i)']])
+    pq.clear()
 
-print('')
-print('    H(m,n,e,f;i,j,a,b) = <0|e2(m,n,f,e) e(-T) H e(T) e2(a,b,j,i)|0>')
-print('')
+    # set right and left-hand operators
+    pq.set_left_operators([['1']])
+    pq.set_right_operators([['e1(a,i)']])
 
-pq.add_st_operator(1.0,['f'],['t1','t2'])
-pq.add_st_operator(1.0,['v'],['t1','t2'])
+    print('')
+    print('#    H(0;i,a) = <0| e(-T) H e(T) e1(a,i)|0>')
+    print('')
 
-pq.simplify()
-pq.print_fully_contracted()
-pq.clear()
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
 
+    pq.simplify()
+
+    H0s = pq.fully_contracted_strings()
+    H0s = contracted_strings_to_tensor_terms(H0s)
+    for my_term in H0s:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='H0s',
+                                    output_variables=('a', 'i')))
+        print()
+
+    pq.clear()
+
+    # set right and left-hand operators
+    pq.set_left_operators([['e2(m,n,f,e)']])
+    pq.set_right_operators([['1']])
+
+    print('')
+    print('#    H(m,n,e,f;0) = <0|e2(m,n,f,e) e(-T) H e(T) |0>')
+    print('')
+
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
+
+    pq.simplify()
+
+    Hd0 = pq.fully_contracted_strings()
+    Hd0 = contracted_strings_to_tensor_terms(Hd0)
+    for my_term in Hd0:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='Hd0',
+                                    output_variables=('e', 'f', 'm', 'n')))
+        print()
+
+    pq.clear()
+    
+    # set right and left-hand operators
+    pq.set_left_operators([['1']])
+    pq.set_right_operators([['e2(a,b,j,i)']])
+    
+    print('')
+    print('#    H(0;i,j,a,b) = <0| e(-T) H e(T) e2(a,b,j,i)|0>')
+    print('')
+    
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
+    
+    pq.simplify()
+
+    H0d = pq.fully_contracted_strings()
+    H0d = contracted_strings_to_tensor_terms(H0d)
+    for my_term in H0d:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='H0d',
+                                    output_variables=('a', 'b', 'i', 'j')))
+        print()
+
+    pq.clear()
+
+    # set right and left-hand operators
+    pq.set_left_operators([['e1(m,e)']])
+    pq.set_right_operators([['e1(a,i)']])
+
+    print('')
+    print('#    H(m,e;i,a) = <0|e1(m,e) e(-T) H e(T) e1(a,i)|0>')
+    print('')
+
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
+
+    pq.simplify()
+
+    Hss = pq.fully_contracted_strings()
+    Hss = contracted_strings_to_tensor_terms(Hss)
+    for my_term in Hss:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='Hss',
+                                    output_variables=('e', 'm', 'a', 'i')))
+        print()
+
+    pq.clear()
+    
+    # set right and left-hand operators
+    pq.set_left_operators([['e1(m,e)']])
+    pq.set_right_operators([['e2(a,b,j,i)']])
+    
+    print('')
+    print('#    H(m,e;i,j,a,b) = <0|e1(m,e) e(-T) H e(T) e2(a,b,j,i)|0>')
+    print('')
+    
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
+    
+    pq.simplify()
+
+    Hsd = pq.fully_contracted_strings()
+    Hsd = contracted_strings_to_tensor_terms(Hsd)
+    for my_term in Hsd:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='Hsd',
+                                    output_variables=('e', 'm', 'a', 'b', 'i', 'j')))
+        print()
+
+    pq.clear()
+    
+    # set right and left-hand operators
+    pq.set_left_operators([['e2(m,n,f,e)']])
+    pq.set_right_operators([['e1(a,i)']])
+    
+    print('')
+    print('#    H(m,n,e,f;i,a) = <0|e2(m,n,f,e) e(-T) H e(T) e1(a,i)|0>')
+    print('')
+    
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
+    
+    pq.simplify()
+
+    Hds = pq.fully_contracted_strings()
+    Hds = contracted_strings_to_tensor_terms(Hds)
+    for my_term in Hds:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='Hds',
+                                    output_variables=('e', 'f', 'm', 'n', 'a', 'i')))
+        print()
+
+    pq.clear()
+    
+    # set right and left-hand operators
+    pq.set_left_operators([['e2(m,n,f,e)']])
+    pq.set_right_operators([['e2(a,b,j,i)']])
+    
+    print('')
+    print('#    H(m,n,e,f;i,j,a,b) = <0|e2(m,n,f,e) e(-T) H e(T) e2(a,b,j,i)|0>')
+    print('')
+    
+    pq.add_st_operator(1.0,['f'],['t1','t2'])
+    pq.add_st_operator(1.0,['v'],['t1','t2'])
+    
+    pq.simplify()
+
+    Hdd = pq.fully_contracted_strings()
+    Hdd = contracted_strings_to_tensor_terms(Hdd)
+    for my_term in Hdd:
+        print("#\t", my_term)
+        print(my_term.einsum_string(update_val='Hdd',
+                                    output_variables=('e', 'f', 'm', 'n', 'a', 'b', 'i', 'j')))
+        print()
+
+    pq.clear()
+
+if __name__ == "__main__":
+    main()
