e6cd6149d23acaf7a69a62472e3ef40472b8ae83
edeprince3 <eugene.deprince@gmail.com>
2022-12-27 09:23:52 -0500

    fixes ccsd with spin example

--- examples/ccsd_with_spin.py
+++ examples/ccsd_with_spin.py
@@ -39,8 +39,11 @@ pq.add_st_operator(1.0,['v'],['t1','t2'])
 pq.simplify()
 
 # grab list of fully-contracted strings, then print
-pq.set_non_summed_spin_labels([['m', 'a'], ['e', 'a']])
-singles_residual_terms = pq.fully_contracted_strings_with_spin()
+spin_labels = {
+    'e' : 'a',
+    'm' : 'a',
+}
+singles_residual_terms = pq.fully_contracted_strings_with_spin(spin_labels)
 for my_term in singles_residual_terms:
     print(my_term)
 
@@ -49,8 +52,11 @@ print('#    < 0 | m* e e(-T) H e(T) | 0> (bb):')
 print('')
 
 # grab list of fully-contracted strings, then print
-pq.set_non_summed_spin_labels([['m', 'b'], ['e', 'b']])
-singles_residual_terms = pq.fully_contracted_strings_with_spin()
+spin_labels = {
+    'e' : 'b',
+    'm' : 'b',
+}
+singles_residual_terms = pq.fully_contracted_strings_with_spin(spin_labels)
 for my_term in singles_residual_terms:
     print(my_term)
 
@@ -70,8 +76,13 @@ pq.add_st_operator(1.0,['v'],['t1','t2'])
 pq.simplify()
 
 # grab list of fully-contracted strings, then print
-pq.set_non_summed_spin_labels([['m', 'a'], ['n', 'a'], ['e', 'a'], ['f', 'a']])
-doubles_residual_terms = pq.fully_contracted_strings_with_spin()
+spin_labels = {
+    'e' : 'a',
+    'f' : 'a',
+    'm' : 'a',
+    'n' : 'a'
+}
+doubles_residual_terms = pq.fully_contracted_strings_with_spin(spin_labels)
 for my_term in doubles_residual_terms:
     print(my_term)
 
@@ -80,8 +91,13 @@ print('#    < 0 | m* n* f e e(-T) H e(T) | 0> (bbbb):')
 print('')
 
 # grab list of fully-contracted strings, then print
-pq.set_non_summed_spin_labels([['m', 'b'], ['n', 'b'], ['e', 'b'], ['f', 'b']])
-doubles_residual_terms = pq.fully_contracted_strings_with_spin()
+spin_labels = {
+    'e' : 'b',
+    'f' : 'b',
+    'm' : 'b',
+    'n' : 'b'
+}
+doubles_residual_terms = pq.fully_contracted_strings_with_spin(spin_labels)
 for my_term in doubles_residual_terms:
     print(my_term)
 
@@ -90,8 +106,13 @@ print('#    < 0 | m* n* f e e(-T) H e(T) | 0> (abab):')
 print('')
 
 # grab list of fully-contracted strings, then print
-pq.set_non_summed_spin_labels([['m', 'a'], ['n', 'b'], ['e', 'a'], ['f', 'b']])
-doubles_residual_terms = pq.fully_contracted_strings_with_spin()
+spin_labels = {
+    'e' : 'a',
+    'f' : 'b',
+    'm' : 'a',
+    'n' : 'b'
+}
+doubles_residual_terms = pq.fully_contracted_strings_with_spin(spin_labels)
 for my_term in doubles_residual_terms:
     print(my_term)
 
