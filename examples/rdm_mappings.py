34fbaeed4b16af0f2242c006614090be0f7c2226
edeprince3 <eugene.deprince@gmail.com>
2023-01-09 16:45:02 -0500

    removing dependence on set_string add_string from tests

--- examples/rdm_mappings.py
+++ examples/rdm_mappings.py
@@ -4,41 +4,30 @@ Example for vacuum normal ordering the T2 operator for 2-RDM theory
 import pdaggerq
 
 def main():
+
     print("T2 mappings")
     pq = pdaggerq.pq_helper('true')
-    pq.set_string(['i*','j*','k','n*','m', 'l'])
-    pq.add_new_string()
-    pq.set_string(['n*','m','l', 'i*','j*', 'k'])
-    pq.add_new_string()
+    pq.add_operator_product(1.0, ['a*(i)', 'a*(j)', 'a(k)', 'a*(n)', 'a(m)', 'a(l)'])
+    pq.add_operator_product(1.0, ['a*(n)', 'a(m)', 'a(l)', 'a*(i)', 'a*(j)', 'a(k)'])
     pq.simplify()
     pq.print()
     pq.clear()
 
     print("T1 mappings")
-    pq = pdaggerq.pq_helper('true')
-    pq.set_string(['i*','j*','k*','n','m', 'l'])
-    pq.add_new_string()
-    pq.set_string(['n','m','l', 'i*','j*', 'k*'])
-    pq.add_new_string()
+    pq.add_operator_product(1.0, ['a*(i)', 'a*(j)', 'a*(k)', 'a(n)', 'a(m)', 'a(l)'])
+    pq.add_operator_product(1.0, ['a(n)', 'a(m)', 'a(l)', 'a*(i)', 'a*(j)', 'a*(k)'])
     pq.simplify()
     pq.print()
     pq.clear()
 
     print("Q -> D")
-    pq = pdaggerq.pq_helper('true')
-
-    pq.set_string(['i', 'j', 'k*', 'l*'])
-    pq.add_new_string()
-
+    pq.add_operator_product(1.0, ['a(i)', 'a(j)', 'a*(k)', 'a*(l)'])
     pq.simplify()
     pq.print()
-
     pq.clear()
 
     print("G -> D")
-    pq = pdaggerq.pq_helper('true')
-    pq.set_string(['i*', 'j', 'k*', 'l'])
-    pq.add_new_string()
+    pq.add_operator_product(1.0, ['a*(i)', 'a(j)', 'a*(k)', 'a(l)'])
     pq.simplify()
     pq.print()
     pq.clear()
