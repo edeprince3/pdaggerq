14d4df48ecaab9eefbfdad079715998c56e50f06
edeprince3 <eugene.deprince@gmail.com>
2023-06-15 22:55:23 -0400

    integrals::to_string() should exit with an error if it doesn't recognize the integral type

--- pdaggerq/pq_tensor.cc
+++ pdaggerq/pq_tensor.cc
@@ -357,6 +357,11 @@ std::string integrals::to_string(std::string symbol) {
             + ","
             + labels[1]
             + ")";
+    }else {
+        printf("\n");
+        printf("    unknown integral type: %s\n", symbol.c_str());
+        printf("\n");
+        exit(1);
     }
 
     return val;
