afb84a86d521185cf19a052208cf3dc19b6cc7d5
Marclie <mliebenthal@fsu.edu>
2023-12-07 11:07:45 -0500

    changed line hash

--- pq_graph/include/line.hpp
+++ pq_graph/include/line.hpp
@@ -223,18 +223,16 @@ namespace pdaggerq {
     struct LineHash {
         uint_fast16_t operator()(const Line &line) const {
 
-            uint_fast16_t hash = 0;
+            // only use first character (8 bits)
+            uint_fast16_t hash = line.label_[0] << 1;
 
             // we can store each boolean as a bit in an integral type (4 bits)
-            hash = (hash << 1) | line.o_;
+            hash |= line.o_;
             hash = (hash << 1) | line.a_;
             hash = (hash << 1) | line.sig_;
             hash = (hash << 1) | line.den_;
 
-            // because a char is 8 bits, we shift by 8 (8 bits; 12 total)
-            hash = (hash << 8) | line.label_[0];
-
-            // return the hash
+            // return the hash (12 bits)
             return hash;
         }
     }; // struct LineHash
@@ -270,11 +268,10 @@ namespace pdaggerq {
         uint_fast8_t operator()(const Line &line) const {
 
             //  We do not care about the label for this hash function.
-            uint_fast8_t hash = 0;
+            uint_fast8_t hash = line.o_ << 1;
 
             // store each boolean as a bit in an integral type
-            hash = (hash << 1) | line.o_;
-            hash = (hash << 1) | line.a_;
+            hash |= line.a_;
             hash = (hash << 1) | line.sig_;
             hash = (hash << 1) | line.den_;
 
