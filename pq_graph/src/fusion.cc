d6c28551c117ee4d4954d9bddbeba04eb50cb9fc
MarcLie <mliebenthal@fsu.edu>
2024-11-08 14:23:52 -0500

    prevent unsetting of intermediates when merging together. This step should reproduced when pruning.

--- pq_graph/src/fusion.cc
+++ pq_graph/src/fusion.cc
@@ -564,7 +564,6 @@ struct LinkMerger {
                 // build merged vertex
                 MutableVertexPtr merged_vertex = target_infos[i].link->shallow();
                 long max_id = link_tracker_.max_ids_[target_link->type()];
-                merged_vertex->id() = -1;
 
                 Term *target_term = target_infos[i].term;
                 string merged_pq = target_term->original_pq_;
@@ -572,7 +571,6 @@ struct LinkMerger {
                     MutableLinkagePtr target_vertex = as_link(merge_info[i].link->shallow());
                     Term *merge_term = merge_info[i].term;
                     max_id = std::max(max_id, target_vertex->id());
-                    target_vertex->id() = -1;
 
                     // get ratio of coefficients
                     double ratio = merge_term->coefficient_ / target_term->coefficient_;
