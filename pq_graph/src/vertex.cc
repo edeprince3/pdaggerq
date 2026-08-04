f439cc119de9ce5721ab4f077b446d87f04b2007
Marcus Liebenthal <markliebent@gmail.com>
2026-08-04 19:31:51 -0400

    Enhance label handling in vertex processing: convert to uppercase and adjust counts for repeated lines.

--- pq_graph/src/vertex.cc
+++ pq_graph/src/vertex.cc
@@ -660,8 +660,12 @@ namespace pdaggerq {
 
             while (it != lines_.end()) {
                 // replace the repeated lines with arbitrary lines
-                if (counts[line] != 0)
-                    it->label_[0] += counts[line];
+                if (counts[line] != 0) {
+                    // make uppercase and increment
+                    it->label_[0] -= 'a' - 'A';
+                    it->label_[0] += counts[line]-1;
+                }
+
                 counts[line]++;
 
                 it = std::find(it+1, lines_.end(), line);
@@ -679,8 +683,10 @@ namespace pdaggerq {
             delta_lines.reserve(freq);
             for (uint_fast8_t i = 0; i < freq; i++) {
                 Line new_line = line;
-                if (i != 0)
-                    new_line.label_[0] += i;
+                if (i != 0) {
+                    new_line.label_[0] -= 'a' - 'A';
+                    new_line.label_[0] += i-1;
+                }
                 delta_lines.push_back(new_line);
             }
 
